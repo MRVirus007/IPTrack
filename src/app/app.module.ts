49fcc2e011ffe19c95495a47dec5add455e142b8
MRVirus007 <hackerpc77@gmail.com>
2021-01-15 13:28:11 +0530

    Another

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,20 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { HttpClientModule } from '@angular/common/http';
+import { ApiService } from './services/api.service';
+
+import { AdMobFree } from '@ionic-native/admob-free/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
   providers: [
     StatusBar,
     SplashScreen,
+    AdMobFree,
+    ApiService,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
